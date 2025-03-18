eea8997a5bfd1564f0b318b5341cb1530f6badd4
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-03-18 11:42:37 +0100

    fixed layout pres

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,10 @@ export default function Page() {
     <div className="container mx-auto p-4">
       <div className={` md:p-32 p-7 `}></div>
       <div className="flex justify-center md:justify-start ">
-        <div className="flex flex-col justify-center items-left  mx-4 md:mx-0">
-          <h1 className="text-4xl font-semibold">
-            Hi, I'm <span className="gradient-text">Benjamin Roget</span>
-          </h1>
+        <div className="flex flex-col justify-center text-center md:text-left mx-4 md:mx-0">
+            <h1 className="text-2xl md:text-4xl font-semibold">
+              Hi, I'm <span className="gradient-text">Benjamin Roget</span>
+            </h1>
           <p className="text-lg text-gray-500">I'm a IT student from France</p>
         </div>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -15,53 +15,65 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex justify-between items-center p-4 px-8 bg-[--black-transparent] shadow-md sticky top-0 w-full z-10">
-      <div className="text-white text-lg font-semibold">Portfolio</div>
+    <nav className='sticky top-0 z-10 flex w-full items-center justify-between bg-[--black-transparent] p-4 px-8 shadow-md'>
+      <div className='text-lg font-semibold text-white'>Portfolio</div>
 
-      <div className="md:hidden cursor-pointer" onClick={toggleMenu}>
+      <div className='cursor-pointer md:hidden' onClick={toggleMenu}>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+          xmlns='http://www.w3.org/2000/svg'
+          className='h-6 w-6 text-white'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke='currentColor'
           strokeWidth={2}
         >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M4 6h16M4 12h16m-7 6h7'
+          />
         </svg>
       </div>
 
-      <ul className={`flex-col md:flex md:flex-row md:space-x-6 absolute md:static bg-[--black-transparent] md:bg-transparent transition-all duration-300 ${isOpen ? "top-16" : "top-[-200px]"} md:top-0`}>
-        <li>
+      <ul
+        className={`absolute flex-col bg-[--black-transparent] transition-all duration-300 md:static md:flex md:flex-row md:space-x-6 md:bg-transparent ${isOpen ? 'top-16' : 'top-[-200px]'} md:top-0`}
+      >
+        <li
+          className={`group ${activeLink === 'Home' ? 'font-bold' : ''} w-16 flex align-middle justify-center`}
+        >
           <a
-            href="#"
+            href='#'
             onClick={() => handleLinkClick('Home')}
-            className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Home' ? 'font-bold' : ''}`}
+            className={`block text-white transition duration-300 hover:text-gray-300`}
           >
             Home
           </a>
         </li>
-        <li>
+        <li
+          className={`group ${activeLink === 'Technologies' ? 'font-bold' : ''} w-16  flex align-middle justify-center`}
+        >
           <a
-            href="#techContainer"
+            href='#techContainer'
             onClick={() => handleLinkClick('Technologies')}
             className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Technologies' ? 'font-bold' : ''}`}
           >
-            Technologies
+            Techno
           </a>
         </li>
-        <li>
+        <li
+          className={`group ${activeLink === 'Projects' ? 'font-bold' : ''} w-16  flex align-middle justify-center`}
+        >
           <a
-            href="#projects"
+            href='#projects'
             onClick={() => handleLinkClick('Projects')}
             className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Projects' ? 'font-bold' : ''}`}
           >
             Projects
           </a>
         </li>
-        <li>
+        <li className={`group  ${activeLink === 'Contact' ? 'font-bold' : ''} w-16  flex align-middle justify-center`}>
           <a
-            href="#contact"
+            href='#contact'
             onClick={() => handleLinkClick('Contact')}
             className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Contact' ? 'font-bold' : ''}`}
           >
@@ -70,7 +82,7 @@ const Navbar = () => {
         </li>
       </ul>
     </nav>
-  );
+  )
 };
 
 export default Navbar;
