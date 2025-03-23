e7741b827f075b335142bf4e822d7ff48b7c80f6
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-03-23 23:38:52 +0100

    added footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,12 +7,14 @@ import {
 import { Badge } from '@/components/ui/badge'
 import { ProjectCard } from '@/components/ProjectCard'
 import { projects, techno } from '@/app/(root)/var'
+import { handleSmoothScroll } from '@/components/Header'
+import { Separator } from '@/components/ui/separator'
 
 export default function Page() {
 
   return (
     <section className='h-full w-screen bg-[--white-transparent]'>
-      <div className='container mx-auto h-full' id={'home'}>
+      <div className='container mx-auto h-full' id={'overview'}>
         <div className={`pt-28 md:pt-60`}></div>
         <div className='flex justify-center md:justify-start'>
           <div className='mx-4 flex flex-col justify-center text-center md:mx-0 md:text-left'>
@@ -33,85 +35,58 @@ export default function Page() {
           </div>
         </div>
         <SmallSocialBtn />
-        <div className={`pt-12`}></div>
-        <a
-          href={`#presentation`}
-          className={`hidden justify-center align-middle md:flex md:justify-start`}
-        >
-          <Button
-            className={`relative rounded p-2 text-white shadow hover:bg-neutral-900`}
-          >
-            <h2>
-              See my presentation
-              <span
-                className={`absolute right-[-2] top-[-2] animate-ping rounded-full bg-red-600 p-1 opacity-75`}
-              ></span>
-              <span className='absolute right-[-2] top-[-2] rounded-full bg-red-400 p-1'></span>
-            </h2>
-          </Button>
-        </a>
       </div>
 
-      <div className={`pt-16 md:pt-96`}></div>
-      <div className='container mx-auto' id={'presentation'}>
-        <div className={'align-center flex flex-col justify-center md:pt-32'}>
-          <h1 className='text-2xl font-semibold md:text-4xl'>Presentation</h1>
-          <section className={`pt-12`}></section>
-          <Card className={`bg-white shadow-md`}>
-            <CardContent className={'pt-3'}>
-              <div
-                className={
-                  'align-center flex flex-col justify-center md:flex-row'
-                }
-              >
-                <section className={'md:w-1/2 flex align-center justify-center'}>
-                  <p className={'text-justify'}>
-                    Currently a second-year student in a Bachelor's program in
+      <div className='container mx-auto'>
+        <div className='flex flex-col items-center justify-center md:pt-32'>
+          <section className='pt-12' />
+          <Card className='bg-white shadow-md'>
+            <CardContent className='pt-3'>
+              <div className='flex flex-col items-center justify-center md:flex-row'>
+                <article className='flex items-center justify-center md:w-1/2 pr-2'>
+                  <p className='text-justify'>
+                    Currently a second-year student in a Bachelor&#39;s program in
                     Computer Science, I am passionate about software development
                     and system administration. I have acquired skills in Java,
-                    Python, C, HTML, CSS, and JavaScript, as well as experience
-                    with tools such as Docker, Git, IntelliJ IDEA, PostgreSQL,
-                    and Visual Studio Code. I also have hands-on experience in
+                    Python, C, and JavaScript/TypeScript, as well as experience
+                    with tools such as Docker, Podman, git, I use also frameworks like Next.js, and TailwindCSS.
+                    I also have hands-on experience in
                     server configuration and database management.
                   </p>
-                </section>
-                <section className={`grid md:w-1/2 grid-cols-3 gap-4 md:pl-3 pt-3`}>
-                  {techno.map((tech, index) => {
-                    return (
-                      <Badge
-                        key={index}
-                        className='rounded-lg bg-neutral-100 p-1 text-sm text-neutral-900'
-                      >
-                        <img
-                          src={tech.svg}
-                          className='h-8 w-8 pr-1'
-                          alt={tech.name}
-                        />
-                        {tech.name}
-                      </Badge>
-                    )
-                  })}
+
+                </article>
+                <section className='grid grid-cols-3 gap-4 pt-3 md:w-1/2 md:pl-2 border-l-2 border-neutral-900'>
+                  {techno.map((tech, index) => (
+                    <Badge
+                      key={index}
+                      className='rounded-lg bg-neutral-100 p-1 text-sm text-neutral-900'
+                    >
+                      <img
+                        src={tech.svg}
+                        className='h-8 w-8 pr-1'
+                        alt={tech.name}
+                      />
+                      {tech.name}
+                    </Badge>
+                  ))}
                 </section>
               </div>
             </CardContent>
           </Card>
-          <a
-            href={`#projects`}
-            className={`hidden justify-center align-middle md:flex md:justify-start pt-6`}
-          >
-            <Button
-              className={`relative rounded p-2 text-white shadow hover:bg-neutral-900`}
-            >
-              <h2>
-                See my projects
-                <span
-                  className={`absolute right-[-2] top-[-2] animate-ping rounded-full bg-red-600 p-1 opacity-75`}
-                ></span>
-                <span className='absolute right-[-2] top-[-2] rounded-full bg-red-400 p-1'></span>
-              </h2>
-            </Button>
-          </a>
         </div>
+        <a
+          href='#projects'
+          onClick={handleSmoothScroll}
+          className='hidden justify-start pt-6 md:flex'
+        >
+          <Button className='relative rounded p-2 text-white shadow hover:bg-neutral-900'>
+            <h2>
+              See my projects
+              <span className='absolute right-[-2] top-[-2] animate-ping rounded-full bg-red-600 p-1 opacity-75'></span>
+              <span className='absolute right-[-2] top-[-2] rounded-full bg-red-400 p-1'></span>
+            </h2>
+          </Button>
+        </a>
       </div>
 
       <div className='container mx-auto' id={'project'}>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,55 +1,57 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react';
+
+export const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement>) => {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute('href');
+  if (!targetId) return;
+  const targetElement = document.querySelector(targetId);
+  if (!targetElement) return;
+
+  targetElement.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+};
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [activeLink, setActiveLink] = useState('Home');
-
-  const handleLinkClick = (link:string) => {
-    setActiveLink(link);
-    setIsOpen(false); // Close the menu on link click
-  };
-
-  const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    event.preventDefault()
-    const targetId = event.currentTarget.getAttribute('href') as string
-    const targetElement = document.querySelector(targetId)
-    if (!targetElement) return
-    targetElement.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    })
-  }
+  const [activeLink, setActiveLink] = useState('overview');
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  useEffect(() => {
-    // get all a link and apply the same scroll effect
-    const localALinks = document.querySelectorAll('a');
-    const array = [...localALinks];
-    array.filter(a => {
-      const href = a.getAttribute('href');
-      if (!href) {
-        return false;
-      }
+  const sections = ['overview', 'projects', 'contact'];
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
 
-      return href.startsWith(window.location.origin);
-    })
-    console.log(array)
-    array.forEach((a)=> {
-      a.addEventListener("click", (event)=> {
-        handleLinkClick((event.currentTarget)?.getAttribute('href') as string);
-        handleSmoothScroll(event);
-      });
-    })
-  }, [])
+    sections.forEach((sectionId) => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPosition >= sectionTop - 50 && scrollPosition < sectionTop + sectionHeight) {
+          setActiveLink(sectionId);
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    // Add scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <nav className='sticky z-10 top-0 flex w-full items-center justify-between bg-neutral-900 p-4 px-8 shadow-md'>
       <div className='text-lg font-semibold text-white'>Portfolio</div>
-
       <div className='cursor-pointer md:hidden' onClick={toggleMenu}>
         <svg
           xmlns='http://www.w3.org/2000/svg'
@@ -66,69 +68,23 @@ const Navbar = () => {
           />
         </svg>
       </div>
-
       <ul
         className={`absolute flex-col bg-[--black-transparent] transition-all duration-300 md:static md:flex md:flex-row md:space-x-6 md:bg-transparent ${isOpen ? 'top-16' : 'top-[-200px]'} md:top-0`}
       >
-        <li
-          className={`group ${activeLink === 'Home' ? 'font-bold' : ''} flex w-16 justify-center align-middle`}
-        >
-          <a
-            // scroll to div with id home
-            onClick={(event) => {
-              handleLinkClick('Home');
-              handleSmoothScroll(event);
-            }}
-            href='#home'
-            className={`block text-white transition duration-300 hover:text-gray-300`}
-          >
-            Home
-          </a>
-        </li>
-        <li
-          className={`group ${activeLink === 'Technologies' ? 'font-bold' : ''} flex w-16 justify-center align-middle`}
-        >
-          <a
-            onClick={(event) => {
-              handleLinkClick('Technologies');
-              handleSmoothScroll(event);
-            }}
-            href={'#technologies'}
-            className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Technologies' ? 'font-bold' : ''}`}
-          >
-            Techno
-          </a>
-        </li>
-        <li
-          className={`group ${activeLink === 'Projects' ? 'font-bold' : ''} flex w-16 justify-center align-middle`}
-        >
-          <a
-            onClick={(event) => {
-              handleLinkClick('Projects');
-              handleSmoothScroll(event);
-            }}
-            href={'#projects'}
-            className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Projects' ? 'font-bold' : ''}`}
-          >
-            Projects
-          </a>
-        </li>
-        <li
-          className={`group ${activeLink === 'Contact' ? 'font-bold' : ''} flex w-16 justify-center align-middle`}
-        >
-          <a
-            onClick={(event) => {
-              handleLinkClick('Contact');
-              handleSmoothScroll(event);
-            }}
-            className={`block text-white transition duration-300 hover:text-gray-300 ${activeLink === 'Contact' ? 'font-bold' : ''}`}
-          >
-            Contact
-          </a>
-        </li>
+        {sections.map((link, index) => (
+          <li key={index} className={`group ${activeLink === link ? 'font-bold' : ''} flex w-16 justify-center align-middle`}>
+            <a
+              onClick={handleSmoothScroll}
+              href={`#${link.toLowerCase()}`}
+              className={`block text-white transition duration-300 hover:text-gray-300`}
+            >
+              {link}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
